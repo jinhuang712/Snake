f52422679dfebf2e3c9ff951c9a77c0d9b6e9765
Jin Huang <jinhuang696@gmail.com>
2019-05-27 14:56:14 +0800

    improved readability of code

--- snake.js
+++ snake.js
@@ -7,60 +7,56 @@ window.onload=function () {
     setInterval(game, 1000/15);
 };
 
-let px, py;     // the position of the head
-let gs, tc;     //
-let ax, ay;     // the position of the `apple`
-let x_v, y_v;   // the position of the velocity with direction on x and y axis
-px = py = 10;
-gs = tc = 20;
-ax = ay = 15;
-x_v = y_v = 0;
+let head_x = 10, head_y = 10;
+let grid_size = 20, tile_count = 20;
+let food_x = 15, food_y = 15;
+let x_velocity = 0, y_velocity = 0;
 let trail = []; // the snakes body
 let tail = 5; // the size of the snake
 
 function game() {
-    px += x_v;
-    py += y_v;
+    head_x += x_velocity;
+    head_y += y_velocity;
     // wrapping, where touching the edge of the game canvas does not kill the snake
     // todo: make snake die when touching the edges
-    if (px < 0) {
-        px = tc - 1;
+    if (head_x < 0) {
+        head_x = tile_count - 1;
     }
-    if (px > tc - 1) {
-        px = 0;
+    if (head_x > tile_count - 1) {
+        head_x = 0;
     }
-    if (py < 0) {
-        py = tc - 1;
+    if (head_y < 0) {
+        head_y = tile_count - 1;
     }
-    if (py > tc - 1) {
-        py = 0;
+    if (head_y > tile_count - 1) {
+        head_y = 0;
     }
     context.fillStyle="black";
     context.fillRect(0, 0, canvas.width, canvas.height);
 
     context.fillStyle="lime";
     for (let i = 0; i < trail.length; i++) {
-        context.fillRect(trail[i].x * gs, trail[i].y * gs, gs - 2, gs - 2);
-        if (trail[i].x === px && trail[i].y === py) {
+        context.fillRect(trail[i].x * grid_size, trail[i].y * grid_size, grid_size - 2, grid_size - 2);
+        if (trail[i].x === head_x && trail[i].y === head_y) {
             // todo: losing animation
             tail = 5;
         }
     }
-    trail.push({x:px, y:py});
+    trail.push({x:head_x, y:head_y});
 
     while (trail.length > tail) {
         trail.shift();
     }
 
-    if (ax === px && ay === py) {
+    if (food_x === head_x && food_y === head_y) {
         tail++;
         // todo: need a while loop to make sure the apple does not spawn on the snake body
-        ax = Math.floor(Math.random() * tc);
-        ay = Math.floor(Math.random() * tc);
+        food_x = Math.floor(Math.random() * tile_count);
+        food_y = Math.floor(Math.random() * tile_count);
     }
 
     context.fillStyle="red";
-    context.fillRect(ax*gs, ay*gs, gs - 2, gs - 2);
+    context.fillRect(food_x * grid_size, food_y * grid_size, grid_size - 2, grid_size - 2);
 }
 
 function keyPush(event) {
@@ -84,11 +80,11 @@ function keyPush(event) {
             y_v_ = 1;
             break;
     }
-    if (x_v === 0 && y_v === 0) {
+    if (x_velocity === 0 && y_velocity === 0) {
         // stub
-    } else if (x_v_ === -x_v || y_v_ === -y_v) {
+    } else if (x_v_ === -x_velocity || y_v_ === -y_velocity) {
         return;
     }
-    x_v = x_v_;
-    y_v = y_v_;
+    x_velocity = x_v_;
+    y_velocity = y_v_;
 }
